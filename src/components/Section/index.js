0974f8f6cde4f3ebeae4c7ba1de1515cbac1cdce
Ishaan Rawat <ishaan@ishaanrawat.com>
2018-11-11 00:14:08 +0530

    Using Route

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,4 +1,5 @@
 import React, {Component, Fragment} from 'react';
+import {Route, Switch} from 'react-router-dom';
 
 import LoginSection from './LoginSection';
 import FacultySection from './FacultySection';
@@ -7,23 +8,13 @@ import StudentSection from './StudentSection';
 class Section extends Component {
 
   render() {
-    let section;
-    const logoutButton = <button onClick={this.props.handleLogout}>Log Out</button>
-    if (this.props.logInfo.isLoggedIn) {
-      switch (this.props.logInfo.loggedInAs) {
-        case "STUDENT":
-          section = <StudentSection />
-          break;
-        case "FACULTY":
-          section = <FacultySection />
-          break;
-        default:
-      }
-    }
     return(
       <Fragment>
-        {section ? section : <LoginSection handleLogin={this.props.handleLogin}/> }
-        {this.props.logInfo.isLoggedIn && logoutButton}
+        <Switch>
+          <Route path="/" exact={true} component={LoginSection} />
+          <Route path="/student" component={StudentSection} />
+          <Route path="/faculty" component={FacultySection} />
+        </Switch>
       </Fragment>
     );
   }
