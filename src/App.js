b647cd1900b8fea032c50873407cbb7d185c9e54
Ishaan Rawat <ishaan@ishaanrawat.com>
2018-11-11 00:13:36 +0530

    Cleared up login stuff

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {BrowserRouter} from 'react-router-dom';
 
 import Header from './components/Header';
 import Section from './components/Section';
@@ -7,43 +8,13 @@ import Footer from './components/Footer';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleLogin = this.handleLogin.bind(this);
-    this.handleLogout = this.handleLogout.bind(this);
-  }
-
-  state = {
-    logInfo: {
-      isLoggedIn: false,
-      loggedInAs: ""
-    }
-  }
-
-  handleLogin(type) {
-    this.setState({
-      logInfo: {
-        isLoggedIn: true,
-        loggedInAs: type
-      }
-    });
-  }
-
-  handleLogout() {
-    this.setState({
-      logInfo: {
-        isLoggedIn: false,
-        loggedInAs: ""
-      }
-    });
-  }
-
   render() {
     return (
       <div className="App">
         <Header />
-        <Section logInfo={this.state.logInfo} handleLogin={this.handleLogin} handleLogout={this.handleLogout}/>
+        <BrowserRouter>
+          <Section />
+        </BrowserRouter>
         <Footer />
       </div>
     );
