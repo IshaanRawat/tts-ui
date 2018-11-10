576339144cb9ab76949938748eb8a7d3c8caf264
Ishaan Rawat <ishaan@ishaanrawat.com>
2018-11-10 23:35:39 +0530

    Added styling

--- src/components/Section/LoginSection/index.js
+++ src/components/Section/LoginSection/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const loginSection = (props) => {
   return(
-    <div className="login-container">
-      <div>Login as</div>
-      <button onClick={() => props.handleLogin("FACULTY")}>Faculty Member</button>
-      <button onClick={() => props.handleLogin("ADMIN")}>Administrator</button>
-      <button onClick={() => props.handleLogin("STUDENT")}>Student</button>
-    </div>
+    <section id="login-section">
+      <div className="section-heading">You are a</div>
+      <div className="chain">
+        <button className="six pockets blue" onClick={() => props.handleLogin("FACULTY")}>Faculty Member</button>
+        <button className="six pockets green" onClick={() => props.handleLogin("STUDENT")}>Student</button>
+      </div>
+    </section>
 
   );
 };
