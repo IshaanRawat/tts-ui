95d32fae0a3ad535e059c520fc8bc7c8944f2fc3
Ishaan Rawat <ishaan@ishaanrawat.com>
2018-11-11 16:53:44 +0530

    Added Timetable when with :id

--- src/components/Section/FacultySection/index.js
+++ src/components/Section/FacultySection/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {Link} from 'react-router-dom';
+import TimeTable from './../../../UI/TimeTable';
 
 const facultySection = (props) => {
+  let result;
+  if(props.match.params.id) {
+    result = (
+      <Fragment>
+        <h2>Time Table</h2>
+        <TimeTable />
+      </Fragment>
+    );
+  } else {
+    result = (
+      <form>
+        <label for="roll">Enter your Faculty ID</label>
+        <input name="roll" type="text" placeholder="Faculty ID" />
+        <input type="submit" className="button green" value="View" />
+      </form>
+    );
+  }
   return (
     <section>
       <h1><Link className="no-decoration" to="/"> &larr; </Link>Faculty Section</h1>
+      {result}
     </section>
   );
 };
--- src/components/Section/StudentSection/index.js
+++ src/components/Section/StudentSection/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {Link} from 'react-router-dom';
+import TimeTable from './../../../UI/TimeTable';
 
 const studentSection = (props) => {
+  let result;
+  if(props.match.params.id) {
+    result = (
+      <Fragment>
+        <h2>Time Table</h2>
+        <TimeTable />
+      </Fragment>
+    );
+  } else {
+    result = (
+      <form>
+        <label for="roll">Enter your Roll number</label>
+        <input name="roll" type="text" placeholder="Roll Number" />
+        <input type="submit" className="button green" value="View" />
+      </form>
+    );
+  }
   return (
     <section>
       <h1><Link className="no-decoration" to="/"> &larr; </Link>Student Section</h1>
+      {result}
     </section>
   );
 };
