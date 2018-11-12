a949c056ef9897941ab7cd148e0c207beb9db9da
Ishaan Rawat <ishaan@ishaanrawat.com>
2018-11-12 13:19:37 +0530

    changed basename

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import Footer from './components/Footer';
 import './App.css';
 
 class App extends Component {
+  
   render() {
+    console.log(process.env.PUBLIC_URL);
     return (
       <div className="App">
         <Header />
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename="/tts-ui/">
           <Section />
         </BrowserRouter>
         <Footer />
