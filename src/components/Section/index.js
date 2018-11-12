4628ae4bd3fe3360532a9713c8f3b69fe329cf4a
Ishaan Rawat <ishaan@ishaanrawat.com>
2018-11-12 10:53:22 +0530

    made login routes changes

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -3,7 +3,9 @@ import {Route, Switch} from 'react-router-dom';
 
 import LoginSection from './LoginSection';
 import FacultySection from './FacultySection';
+import FacultyLoginSection from './FacultySection/login';
 import StudentSection from './StudentSection';
+import StudentLoginSection from './StudentSection/login';
 import AdminLoginSection from './AdminSection/login';
 
 class Section extends Component {
@@ -13,10 +15,12 @@ class Section extends Component {
       <Fragment>
         <Switch>
           <Route path="/" exact={true} component={LoginSection} />
+          <Route path="/student" exact={true} component={StudentLoginSection} />
+          <Route path="/student/login" exact={true} component={StudentLoginSection} />
           <Route path="/student/:id" component={StudentSection} />
-          <Route path="/student" component={StudentSection} />
+          <Route path="/faculty/" exact={true} component={FacultyLoginSection} />
+          <Route path="/faculty/login" exact={true} component={FacultyLoginSection} />
           <Route path="/faculty/:id" component={FacultySection} />
-          <Route path="/faculty" component={FacultySection} />
           <Route path="/admin/login" component={AdminLoginSection} />
         </Switch>
       </Fragment>
